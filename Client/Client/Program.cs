6dbcb6510b5d56efc73ebef3a68176aef9c1b416
phatdinhvn <phatdinhvn@gmail.com>
2018-03-11 00:21:43 +0700

    Truyen tai tep tin - con loi

---
+++ Client/Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6789);
+                EndPoint ep = ip;
+
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                Socket clientNoti = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                client.Connect(ip);
+                clientNoti.Bind(ep);
+                Console.WriteLine("Ket noi den Server thanh cong!");
+                
+                /*
+                string s = "Hello server!";
+                byte[] gui = Encoding.ASCII.GetBytes(s);
+                client.Send(gui);
+                */
+                byte[] nhan = new byte[255];
+                Console.WriteLine("Server ton tai cac files:");
+                while (true)
+                {
+                    clientNoti.ReceiveFrom(nhan,ref ep);
+                    string r = Encoding.ASCII.GetString(nhan);
+                    if (r == "end")
+                        break;
+                    
+                } 
+                
+
+                byte[] clientData = new byte[1024 * 5000];
+                Console.WriteLine("Nhap duong dan luu file: \nVi du: C:/ABC/XYZ/");
+                string receivedPath = Console.ReadLine();
+
+                int receivedBytesLen = client.Receive(clientData);
+
+                int fileNameLen = BitConverter.ToInt32(clientData, 0);
+                string fileName = Encoding.ASCII.GetString(clientData, 4, fileNameLen);
+
+                Console.WriteLine("Client:Ket noi thanh cong.\nFile {0} bat dau nhan.", fileName);
+
+                BinaryWriter bWrite = new BinaryWriter(File.Open(receivedPath + fileName, FileMode.Append)); ;
+                bWrite.Write(clientData, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
+
+                Console.WriteLine("{0} da duoc luu vao duong dan: {1}", fileName, receivedPath);
+
+                bWrite.Close();
+                client.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Loi gui file: |" + ex.Message +"|");
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Collections;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Server dang hoat dong.");
+                IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, 6789);
+                EndPoint ep = ipEnd;
+
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                Socket sockNoti = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                sock.Bind(ipEnd);
+                sockNoti.Bind(ep);
+                sock.Listen(100);
+                //clientSock is the socket object of client, so we can use it now to transfer data to client
+                Socket clientSock = sock.Accept();
+
+
+                //string fileName = "test.txt";// "Your File Name";
+                string filePath = @"..\\..\\Files\";//Your File Path;
+
+                string[] folder = Directory.GetFiles(@"..\\..\\Files\");
+                
+                Console.WriteLine("Client da ket noi! "+folder.Length+" tep tin sau san sang tai: ");
+                for (int i = 0;i<folder.Length;i++)
+                {
+                    string fName = Path.GetFileName(folder[i]);
+                    Console.WriteLine(" - "+ fName);
+                    byte[] gui = Encoding.ASCII.GetBytes(fName);
+                    sockNoti.SendTo(gui,ep);
+                    if (i == folder.Length)
+                        break;
+                    
+                }
+
+                byte[] nhan = new byte[255];
+                sock.Receive(nhan);
+                string r = Encoding.ASCII.GetString(nhan);
+                Console.WriteLine("Client: {0}", r);
+                string fileName = r;
+                //-------------------------------------------------------
+                byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+
+                byte[] fileData = File.ReadAllBytes(filePath + fileName);
+                byte[] clientData = new byte[4 + fileNameByte.Length + fileData.Length];
+                byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
+
+                fileNameLen.CopyTo(clientData, 0);
+                fileNameByte.CopyTo(clientData, 4);
+                fileData.CopyTo(clientData, 4 + fileNameByte.Length);
+
+
+                clientSock.Send(clientData);
+                Console.WriteLine("File:{0} has been sent.", fileName);
+
+                sockNoti.Close();
+                sock.Close();
+                clientSock.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("File Receiving fail." + ex.Message);
+            }
+            /*
+            byte[] nhan = new byte[1024];
+            EndPoint ep = ip;
+            server.ReceiveFrom(nhan, ref ep);
+            string r = Encoding.ASCII.GetString(nhan);
+            Console.WriteLine("Client: {0}", r);
+
+            string s = "Hello Client!";
+            byte[] gui = Encoding.ASCII.GetBytes(s);
+            server.SendTo(gui, ep);
+            
+
+            //server.Close();
+            */
+        }
+    }
+}
